a07b7b133b48fce12b2d537e21e5ad81899409c6
Luisma <luisma@canva.com>
2020-06-24 00:26:18 +1000

    [master] Fix 'styles' import

--- src/options/repo-list.tsx
+++ src/options/repo-list.tsx
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import React, { useEffect, useReducer } from 'react';
--- src/options/repo.tsx
+++ src/options/repo.tsx
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
