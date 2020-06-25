d9b0dfd41366d23ec55cbc5df91ffd9357804ad0
Luisma <luisma@canva.com>
2020-06-25 16:38:40 +1000

    [master] Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,63 +1,58 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
-// import sucrase from '@rollup/plugin-sucrase';
-import { terser } from 'rollup-plugin-terser';
-// import visualizer from 'rollup-plugin-visualizer';
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
-
-// import * as react from 'react';
-// import * as reactIs from 'react-is';
-// import * as propTypes from 'prop-types';
-
-// function createSucrasePlugin() {
-//   return sucrase({
-//     exclude: ['node_modules/**'],
-//     transforms: ['typescript'],
-//   });
-// }
+import typescript from '@rollup/plugin-typescript';
+import compiler from '@ampproject/rollup-plugin-closure-compiler';
+import visualizer from 'rollup-plugin-visualizer';
+import sizes from 'rollup-plugin-sizes';
 
 export default [
   {
-    input: 'build/background.js',
+    input: 'src/background.ts',
     output: {
-      file: 'dist/background.js',
+      dir: 'dist',
       format: 'iife',
+      sourcemap: true,
     },
     plugins: [
+      typescript(),
       resolve(),
-      terser(),
-      // visualizer({ filename: 'background.stats.html', gzipSize: true }),
-      sizeSnapshot({ matchSnapshot: true }),
+      compiler(),
+      visualizer({ filename: 'background.stats.html', gzipSize: true }),
+      sizes(),
     ],
   },
   {
     input: 'build/content-script.js',
     output: {
-      file: 'dist/content-script.js',
+      dir: 'dist',
       format: 'iife',
+      sourcemap: true,
     },
     // plugins: [resolve()],
     plugins: [
+      typescript(),
       resolve(),
-      terser(),
-      // visualizer({ filename: 'content-scripts.stats.html', gzipSize: true }),
-      sizeSnapshot({ matchSnapshot: true }),
+      compiler(),
+      visualizer({ filename: 'content-scripts.stats.html', gzipSize: true }),
+      sizes(),
     ],
   },
   {
     input: 'build/options.js',
     output: {
-      file: 'dist/options.js',
+      dir: 'dist',
       format: 'iife',
+      sourcemap: true,
     },
     plugins: [
+      typescript(),
       replace({'process.env.NODE_ENV': JSON.stringify('production')}),
       resolve(),
       commonjs({ include: 'node_modules/**' }),
-      terser(),
-      // visualizer({ filename: 'options.stats.html', gzipSize: true, }),
-      sizeSnapshot({ matchSnapshot: true }),
+      compiler(),
+      visualizer({ filename: 'options.stats.html', gzipSize: true, }),
+      sizes(),
     ],
   },
 ]
